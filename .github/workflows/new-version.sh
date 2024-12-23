ae064590a5ecdafa9180a84e201e01cdf8302eb2
Julian Beittel <julian@beittel.net>
2024-12-23 14:29:57 +0100

    Added Auto Updater Credits to: omarcresp

--- .github/workflows/new-version.sh
+++
@@ -1,8 +0,0 @@
-upstream=$(curl -s https://api.github.com/repos/zen-browser/desktop/releases/latest | jq -r '.tag_name')
-local=$(cat ./flake.nix | grep -oP 'version = "\K[^"]+')
-
-if [ "$upstream" != "$local" ]; then
-  echo "new_version=true" >> $GITHUB_OUTPUT
-fi
-
-echo "$upstream"
