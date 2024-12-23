25c3483a7b70f7c05ad3f5d41eea5bc61ce8c3d1
Julian Beittel <julian@beittel.net>
2024-12-23 14:27:17 +0100

    Added Auto Updater Credits to: omarcresp

--- flake.nix
+++ flake.nix
@@ -8,16 +8,12 @@
   outputs = { self, nixpkgs }:
     let
       system = "x86_64-linux";
-      version = "1.0.2-b.3";
+      version = "";
       downloadUrl = {
-        "specific" = {
-	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
-	  sha256 = "sha256:0gjrvsq83l6424ijii2w0c43f2nkf6n04hb2bc9wf1yyq7g3s2nc";
-	};
-	"generic" = {
-	  url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.bz2";
-	  sha256 = "sha256:1kv44fkql60rjgqcqsfdhbi4zr8bi91fkswlsk5d6mwj8nw1clmj";
-	};
+	specific.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-specific.tar.bz2";
+	specific.sha256 = "";
+	generic.url = "https://github.com/zen-browser/desktop/releases/download/${version}/zen.linux-generic.tar.bz2";
+	generic.sha256 = "";
       };
 
       pkgs = import nixpkgs {
